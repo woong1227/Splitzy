15154f3102d05398101d8dde6c48051baf66409c
Park Andy <woong1227@gmail.com>
2024-10-16 16:40:56 -0700

    Initial Commit

---
+++ Splitzy/SplitzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitzyApp.swift
+//  Splitzy
+//
+//  Created by Andy Park on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
